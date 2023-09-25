3a6abfa5411834a6e78497e3c47a54d92ebe88bb
panduanugrah12 <anugrahpandu11@gmail.com>
2023-09-25 09:05:50 +0700

    first commit

---
+++ Kalkulator.cs
@@ -0,0 +1,37 @@
+﻿
+    int num1 = 0; int num2 = 0;
+
+    Console.WriteLine("Console Calculator in C#\r");
+    Console.WriteLine("------------------------\n");
+
+    Console.WriteLine("nomor ke 1: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("nomor ke 2: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Pilih opsi yang kamu inginkan:");
+    Console.WriteLine("\t+ - Tambah");
+    Console.WriteLine("\t- - Kurang");
+    Console.WriteLine("\t* - Perkalian");
+    Console.WriteLine("\t/ - Pembagian");
+    Console.Write("Your option? ");
+
+    switch (Console.ReadLine())
+    {
+        case "+":
+            Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+            break;
+        case "-":
+            Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+            break;
+        case "*":
+            Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+            break;
+        case "/":
+            Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+            break;
+    }
+   
+    Console.Write("Press any key to close the Calculator console app...");
+    Console.ReadKey();
